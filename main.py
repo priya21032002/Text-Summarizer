776bf5211e0bbdbc92bd635452ae6b31791eda90
unknown <priyalokhande2103@gmail.com>
2026-03-19 21:18:40 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -12,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
